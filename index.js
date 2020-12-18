c815b025c2a665a4e176f94da32b96fad2ad209d
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-18 01:30:17 -0700

    test

--- index.js
+++ index.js
@@ -8,18 +8,20 @@ const token = process.env.BOT_TOKEN;
 inspirobot.login(token);
 
 inspirobot.on('ready', () => {
-  console.log(`Logged in as ${inspirobot.user.tag}!`);
-  inspirobot.user.setGame('!help');
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity("Brody...", { type: "WATCHING" });
 });
 
 inspirobot.on('message', msg => {
-  if (msg.content === '!inspirome') {
+  if (msg.content === "Inspo?") {
+    msg.channel.send({ files: ["./Assets/kirby_hi.gif"] });
+  }
+  if (msg.content === '!quote') {
     request('http://inspirobot.me/api?generate=true', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         msg.channel.send({
           embed: {
-            color: 3447003,
-            description: "Here's a motivational image for you! :wink:",
+            color: 0xff0000,
             image: {
               url: body
             }
