e67935948bf9c492f6f908f11bd5f486c696d9d0
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-17 19:32:21 -0700

    test

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ bot.on("ready", async () => {
   bot.user.setActivity("Brody...", { type: "WATCHING" });
 });
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://inspirobot.me/");
-  await page.click(".btn-text");
-  await page.waitFor(1500);
-  imgs = await page.$$eval(".generator img[src]", (imgs) =>
-  imgs.map((img) => img.getAttribute("src"))
-  );
-  console.log(imgs);
-  console.log(imgs[0]);
-  await browser.close();
-})();
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto("https://inspirobot.me/");
+//   await page.click(".btn-text");
+//   await page.waitFor(1500);
+//   imgs = await page.$$eval(".generator img[src]", (imgs) =>
+//   imgs.map((img) => img.getAttribute("src"))
+//   );
+//   console.log(imgs);
+//   console.log(imgs[0]);
+//   await browser.close();
+// })();
 
 bot.on("message", async (message) => {
   if (message.content === "Inspo?") {
-    message.channel.send("Hi!")
+    message.reply("Hi!")
   }
   // if (message.content === "!quote") {
   //   console.log("finding pics");
