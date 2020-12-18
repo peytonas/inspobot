004a9de10dbc43fa0fe68922a24201ef40854cad
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-18 01:01:35 -0700

    test

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ bot.on("ready", async () => {
   bot.user.setActivity("Brody...", { type: "WATCHING" });
 });
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://inspirobot.me/");
-  await page.click(".btn-text");
-  await page.waitFor(1500);
-  imgs = await page.$$eval(".generator img[src]", (imgs) =>
-  imgs.map((img) => img.getAttribute("src"))
-  );
-  console.log(imgs);
-  console.log(imgs[0]);
-  await browser.close();
-})();
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto("https://inspirobot.me/");
+//   await page.click(".btn-text");
+//   await page.waitFor(1500);
+//   imgs = await page.$$eval(".generator img[src]", (imgs) =>
+//   imgs.map((img) => img.getAttribute("src"))
+//   );
+//   console.log(imgs);
+//   console.log(imgs[0]);
+//   await browser.close();
+// })();
 
 bot.on("message", async (message) => {
   if (message.content === "Inspo?") {
@@ -31,15 +31,25 @@ bot.on("message", async (message) => {
   }
   if (message.content === "!quote") {
     message.channel.send("Did you know...");
-    message.channel.send(imgs[0]);
-    const embed = new Discord.MessageEmbed()
-      .setTitle("INSPIROBOT")
-      .setThumbnail(
-        "https://inspirobot.me/website/images/inspirobot-dark-green.png"
-      )
-      .setColor(0xff0000)
-      .setImage(imgs[0]);
-    console.log(imgs[0])
+    request('http://inspirobot.me/api?generate=true', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        msg.channel.send({
+          embed: {
+            color: 3447003,
+            description: "Here's a motivational image for you! :wink:",
+            image: {
+              url: body
+            }
+          }
+        });
+    // const embed = new Discord.MessageEmbed()
+    //   .setTitle("INSPIROBOT")
+    //   .setThumbnail(
+    //     "https://inspirobot.me/website/images/inspirobot-dark-green.png"
+    //   )
+    //   .setColor(0xff0000)
+    //   .setImage(imgs[0]);
+    // console.log(imgs[0])
   }
 });
 
