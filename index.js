7281c5fe308a063648dc181acac0596eaeaf8a91
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-17 17:39:17 -0700

    test

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ bot.on("ready", async () => {
   bot.user.setActivity("Brody...", { type: "WATCHING" });
 });
 
-let test = (async () => {
+(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://inspirobot.me/");
   await page.click(".btn-text");
   await page.waitFor(1500);
   imgs = await page.$$eval(".generator img[src]", (imgs) =>
-    imgs.map((img) => img.getAttribute("src"))
+  imgs.map((img) => img.getAttribute("src"))
   );
+  console.log(imgs);
   console.log(imgs[0]);
   await browser.close();
 })();
@@ -29,18 +30,17 @@ bot.on("message", async (message) => {
     message.channel.send("Hi!")
   }
   if (message.content === "!quote") {
-    test;
     console.log("finding pics");
-    const embed = new Discord.MessageEmbed()
-      .setTitle("INSPIROBOT")
-      .setThumbnail(
-        "https://inspirobot.me/website/images/inspirobot-dark-green.png"
-      )
-      .setColor(0xff0000)
-      .setImage(
-        imgs[0]
-      );
-    message.channel.send(embed);
+    // const embed = new Discord.MessageEmbed()
+    //   .setTitle("INSPIROBOT")
+    //   .setThumbnail(
+    //     "https://inspirobot.me/website/images/inspirobot-dark-green.png"
+    //   )
+    //   .setColor(0xff0000)
+    //   .setImage(
+    //     imgs[0]
+    //   );
+    message.channel.send(imgs[0]);
   }
 });
 
