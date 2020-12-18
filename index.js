4c1b45f219ecadacc4fa0e5cc7c022bf16a7b952
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-18 00:35:03 -0700

    test

--- index.js
+++ index.js
@@ -11,37 +11,39 @@ bot.on("ready", async () => {
   bot.user.setActivity("Brody...", { type: "WATCHING" });
 });
 
-// (async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto("https://inspirobot.me/");
-//   await page.click(".btn-text");
-//   await page.waitFor(1500);
-//   imgs = await page.$$eval(".generator img[src]", (imgs) =>
-//   imgs.map((img) => img.getAttribute("src"))
-//   );
-//   console.log(imgs);
-//   console.log(imgs[0]);
-//   await browser.close();
-// })();
+const imgGrab = (async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://inspirobot.me/");
+  await page.click(".btn-text");
+  await page.waitFor(1500);
+  imgs = await page.$$eval(".generator img[src]", (imgs) =>
+  imgs.map((img) => img.getAttribute("src"))
+  );
+  console.log(imgs);
+  console.log(imgs[0]);
+  await browser.close();
+})();
 
 bot.on("message", async (message) => {
   if (message.content === "Inspo?") {
     message.channel.send({ files: ["./Assets/kirby_hi.gif"] });
   }
-  // if (message.content === "!quote") {
-  //   console.log("finding pics");
-  //   // const embed = new Discord.MessageEmbed()
-  //   //   .setTitle("INSPIROBOT")
-  //   //   .setThumbnail(
-  //   //     "https://inspirobot.me/website/images/inspirobot-dark-green.png"
-  //   //   )
-  //   //   .setColor(0xff0000)
-  //   //   .setImage(
-  //   //     imgs[0]
-  //   //   );
-  //   message.channel.send(imgs[0]);
-  // }
+  if (message.content === "!quote") {
+    imgGrab()
+    console.log("finding pics");
+    const embed = new Discord.MessageEmbed()
+      .setTitle("INSPIROBOT")
+      .setThumbnail(
+        "https://inspirobot.me/website/images/inspirobot-dark-green.png"
+      )
+      .setColor(0xff0000)
+      .setImage(
+        imgs[0]
+    );
+    console.log(imgs[0])
+    message.channel.send(imgs[0]);
+  }
 });
 
 bot.login(token);
