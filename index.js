6c1200824558ecc0240970bb12ceb7f5fe21c903
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-18 00:37:18 -0700

    test

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on("message", async (message) => {
     message.channel.send({ files: ["./Assets/kirby_hi.gif"] });
   }
   if (message.content === "!quote") {
-    imgGrab()
+    await imgGrab()
     console.log("finding pics");
     const embed = new Discord.MessageEmbed()
       .setTitle("INSPIROBOT")
