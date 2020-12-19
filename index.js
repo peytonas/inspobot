8b3f7873756d9a661120f3acc3ed83fa98e4f4e6
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-12-18 17:15:19 -0700

    interval set

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ inspirobot.on('message', msg => {
     msg.channel.send({ files: ["./Assets/kirby_hi.gif"] });
   }
   if (msg.content === '!quote') {
-    setTimeout(function () {
+    setInterval(function () {
       request('http://inspirobot.me/api?generate=true', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         msg.channel.send({
@@ -24,18 +24,6 @@ inspirobot.on('message', msg => {
             description: "Did you know...🧐",
             image: {
               url: body
-            }
-          }
-        });
-      }
-      else {
-        var errimage = 'http://inspirobot.me/website/images/inspirobot-dark-green.png';
-        msg.channel.send({
-          embed: {
-            color: 3447003,
-            description: "Unfortunately we can't inspire you at the moment. :frowning2:",
-            image: {
-              url: errimage
             }
           }
         });
